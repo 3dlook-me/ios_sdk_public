72e410330ccd7da8b87eefa2544ed0bacdc8a7d0
Grell Gragham <vrt.epm@protonmail.com>
2025-03-18 18:58:24 +0200

    v0.0.2

---
+++ LookCamera.xcframework/ios-arm64/LookCamera.framework/Headers/LookCamera.h
@@ -0,0 +1,18 @@
+//
+//  LookCamera.h
+//  LookCamera
+//
+//  Created by Viktor Ziukov on 15.12.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LookCamera.
+FOUNDATION_EXPORT double LookCameraVersionNumber;
+
+//! Project version string for LookCamera.
+FOUNDATION_EXPORT const unsigned char LookCameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LookCamera/PublicHeader.h>
+
+
